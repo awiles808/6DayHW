200eb8a8fc50623a63d797943e62fd82bd239bfd
awiles808 <awiles808@gmail.com>
2018-03-27 08:51:40 -0600

    3rd commit - performed operations on the crazy object

--- answers.js
+++ answers.js
@@ -1,21 +1,64 @@
-// Boolean
-// String
-// Object
-// Array
-// Object
-// Object
+// // Boolean
+// // String
+// // Object
+// // Array
+// // Object
+// // Object
 
 
 
-//Take It easy
+// //Take It easy
 
-let Rainbow = ["Red","Yellow","Green","Blue","Orange","Purple"];
-console.log(Rainbow[3]);
+// // let Rainbow = ["Red","Yellow","Green","Blue","Orange","Purple"];
+// // console.log(Rainbow[3]);
 
-let Auburn = {favoriteFood:"Orange Chicken and Fried Rice",
-  hobby:"Snowboarding",
-  currentCity:"Fort Collins",
-  favoriteDatatype:"Objects"
-};
+// // let Auburn = {favoriteFood:"Orange Chicken and Fried Rice",
+// //   hobby:"Snowboarding",
+// //   currentCity:"Fort Collins",
+// //   favoriteDatatype:"Objects"
+// // };
 
-Auburn.hobby;
+// // Auburn.hobby;
+
+
+// Crazy Object
+
+
+// const crazyObject = {
+//   taco: [{meat: 'steak',
+//          cheese: ['panela', 'queso', 'chihuahua']},
+//          {meat: 'chicken',
+//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [{
+//       name: "Jeff",
+//       occupation: "manager"
+//     },
+//     {
+//       name: "funkhauser",
+//       occupation: "tv dude"
+//     },
+//     {
+//       name: "susie",
+//       occupation: "jeffs wife",
+//       favourtieHobby: "Swearing at Larry and Jeff"
+//     },
+//     ]
+//   }
+// }
+// Use crazy Object to log the following.
+
+//     "omg my mouth is burning"
+
+//     "Pretty pretty prettayyyyy good"
+
+//     "Swearing at Larry and Jeff"
+
+//     "Chicken Teriyaki Boyyyyyy"
+
+//     The object the contains the name funkhauser
+
+
