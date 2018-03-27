2197c6ea5e603a6699f3d7a5a2023c0ee253b65d
awiles808 <awiles808@gmail.com>
2018-03-27 10:19:30 -0600

    3rd commit - performed operations on the crazy object

--- answers.js
+++ answers.js
@@ -21,44 +21,7 @@
 // // Auburn.hobby;
 
 
-// Crazy Object
-
-
-// const crazyObject = {
-//   taco: [{meat: 'steak',
-//          cheese: ['panela', 'queso', 'chihuahua']},
-//          {meat: 'chicken',
-//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
-//         ],
-//   larry: {
-//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-//     characters: [{
-//       name: "Jeff",
-//       occupation: "manager"
-//     },
-//     {
-//       name: "funkhauser",
-//       occupation: "tv dude"
-//     },
-//     {
-//       name: "susie",
-//       occupation: "jeffs wife",
-//       favourtieHobby: "Swearing at Larry and Jeff"
-//     },
-//     ]
-//   }
-// }
-// Use crazy Object to log the following.
-
-//     "omg my mouth is burning"
-
-//     "Pretty pretty prettayyyyy good"
-
-//     "Swearing at Larry and Jeff"
-
-//     "Chicken Teriyaki Boyyyyyy"
-
-//     The object the contains the name funkhauser
-
+ Crazy Object
+console.log(crazyObject.taco[1].salsa[5]);
+console.log(crazyObject.larry.quotes[0]);
 
