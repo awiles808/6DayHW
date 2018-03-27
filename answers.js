06572bdbd3f9e98832ca7060b110ba6324eb53ed
awiles808 <awiles808@gmail.com>
2018-03-26 21:06:58 -0600

    2nd commit - created data structures and accessed some data

--- answers.js
+++ answers.js
@@ -1,6 +1,21 @@
-Boolean
-String
-Object
-Array
-Object
-Object
+// Boolean
+// String
+// Object
+// Array
+// Object
+// Object
+
+
+
+//Take It easy
+
+let Rainbow = ["Red","Yellow","Green","Blue","Orange","Purple"];
+console.log(Rainbow[3]);
+
+let Auburn = {favoriteFood:"Orange Chicken and Fried Rice",
+  hobby:"Snowboarding",
+  currentCity:"Fort Collins",
+  favoriteDatatype:"Objects"
+};
+
+Auburn.hobby;
